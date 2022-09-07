129e248d250f97b9c8458028f58ac6d9eef751a8
Nguyen Van Hiep <hiep12092001@gmail.com>
2022-09-07 15:52:09 +0700

    edited

--- function.php
+++ function.php
@@ -1,54 +1,68 @@
 <?php
 
-function add_data ($username, $password2, $conn){
+function add_data($username, $password2, $conn)
+{
     $password = (string)md5($password2);
     $query = 'insert into user(username, password) values (?, ?)';
     $exec = $conn->prepare($query);
-    $exec->bind_param('ss',$username, $password);
+    $exec->bind_param('ss', $username, $password);
     $exec->execute();
     return $exec->affected_rows;
 };
 
-function get_username ($username, $conn){
+function get_username($username, $conn)
+{
     $query = 'select * from user where username=?';
     $exec = $conn->prepare($query);
-    $exec->bind_param('s',$username);
+    $exec->bind_param('s', $username);
     $exec->execute();
     $result = $exec->get_result();
     return $result->fetch_array(MYSQLI_ASSOC);
 }
 
-function get_list ($id, $conn){
+function get_list($id, $conn)
+{
     $query = 'select * from to_do_list where id=?';
     $exec = $conn->prepare($query);
-    $exec->bind_param('i',$id);
+    $exec->bind_param('i', $id);
     $exec->execute();
     $result = $exec->get_result();
     return $result->fetch_all(MYSQLI_ASSOC);
 }
 
-function add_list ($id,$mission,$conn){
+function add_list($id, $mission, $conn)
+{
     $query = 'insert into to_do_list(id, task) values (?, ?)';
     $exec = $conn->prepare($query);
-    $exec->bind_param('is',$id,$mission);
+    $exec->bind_param('is', $id, $mission);
     $exec->execute();
     return $exec->affected_rows;
 }
 
-function del_list($stt, $conn){
+function del_list($stt, $conn)
+{
     $query = 'delete from to_do_list where stt=?';
     $exec = $conn->prepare($query);
-    $exec->bind_param('i',$stt);
+    $exec->bind_param('i', $stt);
     $exec->execute();
     return $exec->affected_rows;
 }
 
-function edit_list($update, $stt, $conn){
+function edit_list($update, $stt, $conn)
+{
     $query = 'update to_do_list set task=? WHERE stt=?';
     $exec = $conn->prepare($query);
-    $exec->bind_param('si',$update,$stt);
+    $exec->bind_param('si', $update, $stt);
     $exec->execute();
     return $exec->affected_rows;
 }
-?>
 
+function get_task_by_stt($stt, $conn)
+{
+    $query = 'select task from to_do_list where stt=?';
+    $exec = $conn->prepare($query);
+    $exec->bind_param('i', $stt);
+    $exec->execute();
+    $result = $exec->get_result();
+    return $result->fetch_array(MYSQLI_ASSOC);
+}
--- index.php
+++ index.php
@@ -1,43 +1,24 @@
 <?php
-$conn = mysqli_connect('127.0.0.1', 'root','', 'webb' );
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-//     }
-// echo 'Connected successfully';
-mysqli_set_charset($conn, "utf8");
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $action = $_POST["action"];
-    if($action === 'register')
-        header("Location: /register.php");
-    else header("Location: /login.php");
+session_start();
+include 'connect.php';
+if (isset($_SESSION['loged']) && $_SESSION['loged'] === true) {
+    die(header('location: admin.php'));
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-<!-- <div class="container">
-		<div class="row">
-			<a href="index.php?page=register" class="btn btn-success">'Đăng ký thành viên'</a>
-		</div>
-		<div class="row">
-		//	<?php
-		//	if(isset($_GET["page"]) && $_GET["page"] == "register")
-		//		include "register.php";
-		//   ?>
-		</div>
-	</div> -->
-<form action="" method="POST">
-    <button type="submit" name="action" value="register">  Register  </button>
-    <button type="submit" name="action" value="login">  Login  </button>
-</form>
-</body>
-</html>
 
+<body>
+    <a href="register.php"><button>Register</button></a>
+    <a href="login.php"><button>Login</button></a>
+</body>
+
+</html>
